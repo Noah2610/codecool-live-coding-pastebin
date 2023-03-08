6cecd52ccf854f934aaf5bee7d92e002774199b0
Noah Rosenzweig <noah.rosenzweig@codecool.com>
2023-03-08 10:06:32 +0100

    server: Add /post endpoint to save a new post

--- server/server.js
+++ server/server.js
@@ -1,12 +1,34 @@
+const fs = require("fs");
 const express = require("express");
+const uuid = require("uuid");
 
 const HOST = "0.0.0.0";
 const PORT = 8090;
 
 const server = express();
 
+server.use(express.text());
+
+function generatePostId() {
+    return uuid.v4();
+}
+
+function savePost(id, body) {
+    const filePath = `posts/${id}.txt`;
+    fs.writeFileSync(filePath, body, "utf8");
+}
+
 server.get("/", (req, res) => {
     res.send("Server is running!");
+});
+
+server.post("/post", (req, res) => {
+    const postBody = req.body;
+    const postId = generatePostId();
+
+    savePost(postId, postBody);
+
+    res.send(postId);
 });
 
 server.listen(PORT, HOST, () => {
