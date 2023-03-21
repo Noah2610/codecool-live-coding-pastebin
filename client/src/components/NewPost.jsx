666172d064c672e186ab9a7197ff7f955ea11c7c
Noah Rosenzweig <noah.rosenzweig@codecool.com>
2023-03-21 10:00:46 +0100

    client: Show new post ID after submit post

--- client/src/components/NewPost.jsx
+++ client/src/components/NewPost.jsx
@@ -2,23 +2,38 @@ import { useState } from "react";
 
 export default function NewPost() {
     const [postBody, setPostBody] = useState("");
+    const [postId, setPostId] = useState(null);
 
-    function onSubmit(event) {
+    async function onSubmit(event) {
         event.preventDefault();
 
-        fetch("http://0.0.0.0:8090/post", {
+        const response = await fetch("http://0.0.0.0:8090/post", {
             method: "POST",
             body: postBody,
         });
+
+        const postId = await response.text();
+
+        setPostBody("");
+        setPostId(postId);
     }
 
     return (
-        <form action="#" onSubmit={onSubmit}>
-            <textarea
-                value={postBody}
-                onChange={(event) => setPostBody(event.target.value)}
-            ></textarea>
-            <button>Submit</button>
-        </form>
+        <>
+            <form action="#" onSubmit={onSubmit}>
+                <textarea
+                    value={postBody}
+                    onChange={(event) => setPostBody(event.target.value)}
+                ></textarea>
+                <button>Submit</button>
+            </form>
+
+            {postId && (
+                <div>
+                    New post created!{" "}
+                    <a href={`http://localhost:8080/p/${postId}`}>{postId}</a>
+                </div>
+            )}
+        </>
     );
 }
