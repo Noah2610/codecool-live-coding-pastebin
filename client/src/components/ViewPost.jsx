f74d8013b20752661db9e0dd934fc60885a755d0
Noah Rosenzweig <noah.rosenzweig@codecool.com>
2023-03-28 09:51:43 +0200

    client: Write ViewPost component which shows a post

--- client/src/components/ViewPost.jsx
+++ client/src/components/ViewPost.jsx
@@ -1,9 +1,23 @@
+import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
 export default function ViewPost() {
-    const params = useParams();
+    const { postId } = useParams();
+    const [postBody, setPostBody] = useState(null);
 
-    console.log(params);
+    async function fetchPost() {
+        const response = await fetch(`http://0.0.0.0:8090/post/${postId}`);
+        const postBody = await response.text();
+        setPostBody(postBody);
+    }
 
-    return <h1>View Post!</h1>;
+    useEffect(() => {
+        fetchPost();
+    }, []);
+
+    if (postBody === null) {
+        return <div>Loading post...</div>;
+    }
+
+    return <textarea readOnly>{postBody}</textarea>;
 }
