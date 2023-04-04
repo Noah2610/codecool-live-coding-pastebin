36538157e3221965f94efe0edb3f5c39c1969e9f
Noah Rosenzweig <noah.rosenzweig@codecool.com>
2023-04-04 09:54:21 +0200

    client: Add copy-to-clipboard button

--- client/src/components/ViewPost.jsx
+++ client/src/components/ViewPost.jsx
@@ -1,14 +1,20 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { useParams } from "react-router-dom";
 
 export default function ViewPost() {
     const { postId } = useParams();
     const [postBody, setPostBody] = useState(null);
+    const textareaRef = useRef();
 
     async function fetchPost() {
         const response = await fetch(`http://0.0.0.0:8090/post/${postId}`);
         const postBody = await response.text();
         setPostBody(postBody);
+    }
+
+    function onCopy() {
+        textareaRef.current.select();
+        document.execCommand("copy");
     }
 
     useEffect(() => {
@@ -19,5 +25,12 @@ export default function ViewPost() {
         return <div>Loading post...</div>;
     }
 
-    return <textarea readOnly>{postBody}</textarea>;
+    return (
+        <>
+            <textarea ref={textareaRef} readOnly>
+                {postBody}
+            </textarea>
+            <button onClick={onCopy}>Copy</button>
+        </>
+    );
 }
